6769dbe929a2df4611d1a926d184ead2174df4eb
Eli Kan <rpk6463@nyu.edu>
2024-11-08 23:09:20 -0500

    Crab Movement Implemented with Delta Time

---
+++ Minit Final Project/Assets/Scripts/Crab.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crab : MonoBehaviour
+{
+    private float x;
+    private float newX;
+    //private float originalY;
+    public float speed;
+    public float minX;
+    public float maxX;
+    private Vector3 newPosition;
+    private float accurateTime;
+    private Vector3 changePosition;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        x = Random.Range(minX, maxX);
+        newPosition = new Vector3(x, this.transform.position.y, this.transform.position.z);
+        this.transform.position = newPosition;
+        //originalY = this.transform.position.y;
+        accurateTime = Time.deltaTime;
+        changePosition = new Vector3 (speed, 0f, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = this.transform.position.x;
+        accurateTime = Time.deltaTime;
+        UpdatePosition();
+    }
+
+    private void UpdatePosition()
+    {
+        newX = x + speed*accurateTime;
+        if(newX <= minX)
+        {
+            speed *= -1;
+            changePosition = new Vector3(speed, 0f, 0f);
+        }
+        else if(newX >= maxX)
+        {
+            speed *= -1;
+            changePosition = new Vector3(speed, 0f, 0f);
+        }
+        else
+        {
+            newPosition += changePosition*accurateTime;
+            this.transform.position = newPosition;
+        }
+    }
+}
