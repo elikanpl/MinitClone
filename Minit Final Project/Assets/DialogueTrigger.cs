3b09b78b31f5ec0ca3f599bada397f2afe229483
Wyatt-Destabelle <106564974+Wyatt-Destabelle@users.noreply.github.com>
2024-12-09 11:25:13 -0500

    Changes to dialogue

--- Minit Final Project/Assets/DialogueTrigger.cs
+++ Minit Final Project/Assets/DialogueTrigger.cs
@@ -9,14 +9,24 @@ public class DialogueTrigger : MonoBehaviour
     // Start is called before the first frame update
     [TextArea(15,20)]
     public String text;
-    
+    public float scrollSpeed = .1f;
+
     public MinitDialogue dialogueController;
 
+    void Start()
+    {
+        dialogueController = FindAnyObjectByType<MinitDialogue>();
+    }
     // Update is called once per frame
     void OnTriggerEnter2D(Collider2D col)
     {
         dialogueController.SetPos(transform.position);
         dialogueController.SetText(text);
-        dialogueController.StartTyping();
+        dialogueController.StartTyping(scrollSpeed);
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        dialogueController.closeText();
     }
 }
